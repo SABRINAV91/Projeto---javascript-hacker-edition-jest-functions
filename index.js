1b99c29d52a983ba55aa2521ef8b32649cd1826b
sabrinav91 <satovieiraferreira@gmail.com>
2025-05-02 16:01:19 -0300

    Javascript-hacker

---
+++ index.js
@@ -0,0 +1,23 @@
+// Função que soma dois números
+function somar() {}
+
+// Função que subtrai dois números
+function subtrair() {}
+
+// Função que multiplica dois números
+function multiplicar() {}
+
+// Função que divide dois números, com verificação de divisão por zero
+function dividir() {}
+
+// Função que verifica se um número é par
+function ehPar() {}
+
+// Exporta todas as funções para serem usadas nos testes
+module.exports = {
+  somar,
+  subtrair,
+  multiplicar,
+  dividir,
+  ehPar,
+};
